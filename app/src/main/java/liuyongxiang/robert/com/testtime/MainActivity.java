dd8306a80cebe7e96a73bcc3bc42ea702ef83331
LiuYongxiang <1239551555@qq.com>
2016-10-13 18:43:30 +0800

    Update MainActivity.java

--- app/src/main/java/liuyongxiang/robert/com/testtime/MainActivity.java
+++ app/src/main/java/liuyongxiang/robert/com/testtime/MainActivity.java
@@ -1,4 +1,4 @@
-package liuyongxiang.robert.com.testtime;
+package liuyongxiang.robert.com.testtime.activities;
 
 import android.app.ActionBar;
 import android.app.Activity;
@@ -17,15 +17,20 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+
+import liuyongxiang.robert.com.testtime.R;
 import liuyongxiang.robert.com.testtime.wheelview.DateUtils;
 import liuyongxiang.robert.com.testtime.wheelview.JudgeDate;
 import liuyongxiang.robert.com.testtime.wheelview.ScreenInfo;
 import liuyongxiang.robert.com.testtime.wheelview.WheelMain;
+import liuyongxiang.robert.com.testtime.wheelview.WheelWeekMain;
 
 public class MainActivity extends Activity implements View.OnClickListener{
     private TextView tv_house_time;
+    private TextView tv_week_house_time;
     private TextView tv_center;
     private WheelMain wheelMainDate;
+    private WheelWeekMain wheelWeekMainDate;
     private String beginTime;
 
     @Override
@@ -39,10 +44,12 @@ public class MainActivity extends Activity implements View.OnClickListener{
 
     private void initEvent() {
         tv_house_time.setOnClickListener(this);
+        tv_week_house_time.setOnClickListener(this);
     }
 
     private void initView() {
         tv_house_time = (TextView) findViewById(R.id.tv_house_time);
+        tv_week_house_time = (TextView) findViewById(R.id.tv_week_house_time);
         tv_center = (TextView) findViewById(R.id.tv_center);
     }
 
@@ -110,7 +117,63 @@ public class MainActivity extends Activity implements View.OnClickListener{
             }
         });
     }
+    public void showWeekBottoPopupWindow() {
+        WindowManager manager = (WindowManager)getSystemService(Context.WINDOW_SERVICE);
+        Display defaultDisplay = manager.getDefaultDisplay();
+        DisplayMetrics outMetrics = new DisplayMetrics();
+        defaultDisplay.getMetrics(outMetrics);
+        int width = outMetrics.widthPixels;
+        View menuView = LayoutInflater.from(this).inflate(R.layout.show_week_popup_window,null);
+        final PopupWindow mPopupWindow = new PopupWindow(menuView, (int)(width*0.8),
+                ActionBar.LayoutParams.WRAP_CONTENT);
+        ScreenInfo screenInfoDate = new ScreenInfo(this);
+        wheelWeekMainDate = new WheelWeekMain(menuView, true);
+        wheelWeekMainDate.screenheight = screenInfoDate.getHeight();
+        String time = DateUtils.currentMonth().toString();
+        Calendar calendar = Calendar.getInstance();
+        if (JudgeDate.isDate(time, "yyyy-MM-DD")) {
+            try {
+                calendar.setTime(new Date(time));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH);
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
+        int hours = calendar.get(Calendar.HOUR_OF_DAY);
+        int minute = calendar.get(Calendar.MINUTE);
+        wheelWeekMainDate.initDateTimePicker(year, month, day, hours,minute);
+        final String currentTime = wheelWeekMainDate.getTime().toString();
+        mPopupWindow.setAnimationStyle(R.style.AnimationPreview);
+        mPopupWindow.setTouchable(true);
+        mPopupWindow.setFocusable(true);
+        mPopupWindow.setBackgroundDrawable(new BitmapDrawable());
+        mPopupWindow.showAtLocation(tv_center, Gravity.CENTER, 0, 0);
+        mPopupWindow.setOnDismissListener(new poponDismissListener());
+        backgroundAlpha(0.6f);
+        TextView tv_cancle = (TextView) menuView.findViewById(R.id.tv_cancle);
+        TextView tv_ensure = (TextView) menuView.findViewById(R.id.tv_ensure);
+        TextView tv_pop_title = (TextView) menuView.findViewById(R.id.tv_pop_title);
+        tv_pop_title.setText("选择起始时间");
+        tv_cancle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                mPopupWindow.dismiss();
+                backgroundAlpha(1f);
+            }
+        });
+        tv_ensure.setOnClickListener(new View.OnClickListener() {
 
+            @Override
+            public void onClick(View arg0) {
+                beginTime = wheelWeekMainDate.getTime().toString();
+                    tv_week_house_time.setText(DateUtils.formateStringH(beginTime,DateUtils.yyyyMMddHHmm));
+                mPopupWindow.dismiss();
+                backgroundAlpha(1f);
+            }
+        });
+    }
     public void backgroundAlpha(float bgAlpha) {
         WindowManager.LayoutParams lp = getWindow().getAttributes();
         lp.alpha = bgAlpha;
@@ -122,6 +185,9 @@ public class MainActivity extends Activity implements View.OnClickListener{
         switch (v.getId()){
             case R.id.tv_house_time:
                 showBottoPopupWindow();
+                break;
+            case R.id.tv_week_house_time:
+                showWeekBottoPopupWindow();
                 break;
         }
     }
